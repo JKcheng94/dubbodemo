8fad5698f79828eaf372ca15cead8e1a53aeb948
JKcheng94 <532166732@qq.com>
2018-10-24 10:44:58 +0800

    删除无效import

--- dubbocon/src/main/java/ch/hao/dubbocon/DubboconApplication.java
+++ dubbocon/src/main/java/ch/hao/dubbocon/DubboconApplication.java
@@ -3,14 +3,13 @@ package ch.hao.dubbocon;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.ComponentScans;
-import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"ch.hao.dubbocon.service"})
 public class DubboconApplication {
     
     public static void main(String[] args) {
+        //测试
         SpringApplication.run(DubboconApplication.class, args);
     }
 }
